073a459c1023bc55f5fcea23dad57cfbac5eb754
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2018-09-12 07:55:57 -0500

    Falta que lea correctamente los saltos de linea

---
+++ analizadorLexico/lexer.py
@@ -0,0 +1,34 @@
+import ply.lex as lex
+
+tokens = [ 'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS' ]
+
+t_ignore = ' \t'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_EQUALS = r':='
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex() # Build the lexer
+
+f = open ('entrada.txt','r')
+op = f.read()
+lex.input(op)
+f.close()
+
+
+while True:
+    tok = lex.token()
+    if not tok: break
+    print str(tok.value) + " - " + str(tok.type)
