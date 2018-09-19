8d89ed0ab23852434ec09e5284fbe4175f343e80
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2018-09-18 21:10:33 -0500

    Lexer con diferentes simbolos

---
+++ analizadorLexico/MyLexer.py
@@ -0,0 +1,35 @@
+import ply.lex as lex
+
+tokens = [ 'NAME','NUMBER','PLUS','MINUS','MULTIPLICA','DIVIDE', 'EQUALS', 'JUMPLINE' ]
+
+t_ignore = ' \t'
+t_PLUS = r'SUM'
+t_MINUS = r'RES'
+t_MULTIPLICA = r'MULTI'
+t_DIVIDE = r'DIV'
+t_EQUALS = r'='
+t_JUMPLINE = r'\n'
+#t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex() # Build the lexer
+
+f = open ('Myentrada.txt','r')
+op = f.read()
+lex.input(op)
+f.close()
+
+while True:
+        tok = lex.token()
+        if not tok: break
+        print (str(tok.value) + " - " + str(tok.type))
